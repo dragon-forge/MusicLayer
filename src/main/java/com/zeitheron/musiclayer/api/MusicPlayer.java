f44d92f5589c5901cfdd1df54ae2e5f7165dee26
Zeitheron <zeitheron.dr@gmail.com>
2020-05-02 02:37:31 +0300

    Made sound categories also listen to master category, and base volume multiplier

--- src/main/java/com/zeitheron/musiclayer/api/MusicPlayer.java
+++ src/main/java/com/zeitheron/musiclayer/api/MusicPlayer.java
@@ -1,16 +1,10 @@
 package com.zeitheron.musiclayer.api;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
-
 import com.zeitheron.hammercore.utils.base.EvtBus;
 import com.zeitheron.musiclayer.MusicLayerMC;
-
 import com.zeitheron.musiclayer.internal.soundlib.FadedRepeatableSound;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.GameSettings;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.SoundCategory;
 import net.minecraftforge.client.event.sound.SoundLoadEvent;
@@ -24,6 +18,8 @@ import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+import java.util.*;
+
 @SideOnly(Side.CLIENT)
 public class MusicPlayer
 {
@@ -34,17 +30,19 @@ public class MusicPlayer
 	private Thread ticker;
 	public FadedRepeatableSound prevSound;
 	public final String name;
-	
+
 	public double fadingSpeed = 0.00125F;
 	public float initialVolume = 0.5F;
-	
+
+	public float categoryVolume = 0.8F;
+
 	public SoundCategory listenCategory = SoundCategory.AMBIENT;
-	
+
 	public MusicPlayer(String name)
 	{
 		this(name, 30);
 	}
-	
+
 	public MusicPlayer(String name, int ticksPerSecond)
 	{
 		this.name = name;
@@ -53,12 +51,12 @@ public class MusicPlayer
 		checkThreadState();
 		MusicLayerMC.proxy.assign(this);
 	}
-	
+
 	private void checkThreadState()
 	{
 		if(ticker != null && ticker.isAlive())
 			return;
-		
+
 		ticker = new Thread(() ->
 		{
 			while(true)
@@ -74,18 +72,18 @@ public class MusicPlayer
 					update();
 			}
 		});
-		
+
 		ticker.setName("MusicLayer#" + name);
 		ticker.start();
 	}
-	
+
 	@SubscribeEvent
 	public void update(ClientTickEvent e)
 	{
 		if(e.phase == Phase.START)
 			checkThreadState();
 	}
-	
+
 	@SideOnly(Side.CLIENT)
 	public void update()
 	{
@@ -99,35 +97,36 @@ public class MusicPlayer
 			} else
 				snd.update();
 		}
-		
+
 		if(!pendingRemove.isEmpty())
 		{
 			allTickedSounds.removeAll(pendingRemove);
 			pendingRemove.clear();
 		}
-		
+
 		try
 		{
 			if(prevSound != null)
 			{
 				allTickedSounds.add(prevSound);
-				prevSound.setVolumeD(Minecraft.getMinecraft().gameSettings.getSoundLevel(listenCategory));
+				GameSettings settings = Minecraft.getMinecraft().gameSettings;
+				prevSound.setVolumeD(settings.getSoundLevel(listenCategory) * settings.getSoundLevel(SoundCategory.MASTER) * categoryVolume);
 				prevSound.disposeWhenNotHearable = false;
-				
+
 				if(prevSound.getVolume() == 0F)
 				{
 					// Called when sound is done playing
 					EvtBus.postSafe(BUS, new UpdateAlternativeMusicEvent(this));
-					
+
 					prevSound.dispose();
 					prevSound = null;
 				}
-				
+
 				if(prevSound.isDonePlaying())
 				{
 					// Called when sound is done playing
 					EvtBus.postSafe(BUS, new UpdateAlternativeMusicEvent(this));
-					
+
 					prevSound.dispose();
 					allTickedSounds.remove(prevSound);
 					prevSound = null;
@@ -136,13 +135,13 @@ public class MusicPlayer
 		} catch(NullPointerException npe)
 		{
 		}
-		
+
 		try
 		{
 			GetMusicEvent gme = new GetMusicEvent(Minecraft.getMinecraft().player, null, this, "Nothing");
 			getDefMusic(gme);
 			BUS.post(gme);
-			
+
 			if(gme.getMusic() != null)
 			{
 				if(prevSound == null)
@@ -155,13 +154,13 @@ public class MusicPlayer
 				{
 					/** Update possible alternative music */
 					EvtBus.postSafe(BUS, new UpdateAlternativeMusicEvent(this));
-					
+
 					gme = new GetMusicEvent(Minecraft.getMinecraft().player, null, this, "Nothing");
 					getDefMusic(gme);
 					BUS.post(gme);
-					
+
 					String soundName = gme.getName();
-					
+
 					prevSound.fadeOut();
 					prevSound = new FadedRepeatableSound(gme.getMusic(), initialVolume, soundName);
 					prevSound.fadeSpeed = fadingSpeed;
@@ -173,12 +172,12 @@ public class MusicPlayer
 				prevSound.fadeOut();
 				prevSound = null;
 			}
-			
+
 		} catch(Throwable err)
 		{
 		}
 	}
-	
+
 	@SubscribeEvent
 	public void soundSystemUnload(SoundLoadEvent e)
 	{
@@ -188,7 +187,7 @@ public class MusicPlayer
 
 		EvtBus.postSafe(BUS, new UpdateAlternativeMusicEvent(this));
 	}
-	
+
 	@SubscribeEvent
 	@SideOnly(Side.CLIENT)
 	public void login(PlayerLoggedInEvent event)
@@ -199,7 +198,7 @@ public class MusicPlayer
 			prevSound.fadeOut();
 		prevSound = null;
 	}
-	
+
 	@SubscribeEvent
 	@SideOnly(Side.CLIENT)
 	public void logout(PlayerLoggedOutEvent event)
@@ -213,13 +212,13 @@ public class MusicPlayer
 		}
 		prevSound = null;
 	}
-	
+
 	private void getDefMusic(GetMusicEvent gme)
 	{
 		gme.setName(null);
 		gme.setMusic(null);
 	}
-	
+
 	@Override
 	public boolean equals(Object obj)
 	{
---
+++ ↑ Maven.bat
@@ -0,0 +1,2 @@
+java -jar gradle/FGMavenGen.jar -artifact MusicLayer-1.12.2 -group tk.zeitheron.MusicLayer -src build/libs -mvn D:\Code\Minecraft\Maven -version.props build.txt -version.props.name mod_version
+pause
